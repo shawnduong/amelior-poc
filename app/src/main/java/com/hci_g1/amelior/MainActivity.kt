ee12e9747d96029052a6b182a000e0667e6669a0
shawnduong <shawnduong@protonmail.com>
2022-03-18 07:51:24 -0700

    Created basic interface.

---
+++ app/src/main/java/com/hci_g1/amelior/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.hci_g1.amelior
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity: AppCompatActivity()
+{
+	fun onCreate()
+	{
+		setContentView(R.layout.main_activity)
+	}
+}
