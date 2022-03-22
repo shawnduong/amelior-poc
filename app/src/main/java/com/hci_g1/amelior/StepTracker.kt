50ce0dacbc83f6c980088122b0d7144172228dad
Charison Gill-Branion <cgill-branion@ucmerced.edu>
2022-03-22 16:14:07 -0700

    Add status string to trace service callbacks

--- app/src/main/java/com/hci_g1/amelior/StepTracker.kt
+++ app/src/main/java/com/hci_g1/amelior/StepTracker.kt
@@ -5,12 +5,26 @@ import android.content.Intent
 import android.os.Binder
 import android.os.IBinder
 
+/* NOTE: Services use the main thread by default
+*   May need to start a thread here so we don't accidentally block UI operations
+*   See https://developer.android.com/guide/components/services#CreatingAService
+*/
+
 class StepTracker : Service() {
+    /** DATA **/
+    private var status: String = "Lorem Ipsum"
+    private var demand: Int = 0
+
+    private fun getStatus(): String
+    {
+        return status
+    }
+
     /** INTERFACE **/
-    // Provide a reference to to this StepTracker to clients
+    // Provide the status string to clients
     inner class STBinding : Binder()
     {
-        fun getStepTracker() : StepTracker { return this@StepTracker }
+        fun getStepTracker() : String { return this@StepTracker.getStatus() }
     }
 
     // Instantiate the interface to return it to clients
@@ -19,6 +33,7 @@ class StepTracker : Service() {
     /** LIFECYCLE CALLBACKS **/
     override fun onCreate() {
         super.onCreate()
+        status = "Created new StepTracker\n"
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
@@ -27,6 +42,15 @@ class StepTracker : Service() {
                 "Either stopSelf() locally" +
                 "Or stopService() in clients")
         return super.onStartCommand(intent, flags, startId)
+
+        status += "Started StepTracker (request: $startId)\n"
+
+        TODO(reason = "Implement safe start-ups")
+        /*
+            flags and startId can be used to gracefully handle
+            our service getting killed while starting.
+            Come back around after main functionality is done.
+         */
     }
 
     override fun onDestroy() {
@@ -34,17 +58,26 @@ class StepTracker : Service() {
     }
 
     /** BINDING CALLBACKS **/
-    override fun onBind(p0: Intent?): IBinder? {
+    override fun onBind(intent: Intent?): IBinder? {
+        demand++
+        status += "New Client\nNow bound to $demand clients\n"
+
         TODO(reason = "Implement onServiceConnected() in clients")
         return binding
     }
 
     override fun onUnbind(intent: Intent?): Boolean {
+        demand--
+        status = "Client Unbound\nNow bound to $demand clients\n"
+
         //return super.onUnbind(intent)
         return true
     }
 
     override fun onRebind(intent: Intent?) {
+        demand++
+        status = "Client Rebound\nNow bound to $demand clients\n"
+
         super.onRebind(intent)
     }
 }
