598bd28d356089f07add2165c7e2410a065549f1
Charison Gill-Branion <cgill-branion@ucmerced.edu>
2022-03-24 22:53:24 -0700

    Implement private Fragment View member var

--- app/src/main/java/com/hci_g1/amelior/StepDisplay.kt
+++ app/src/main/java/com/hci_g1/amelior/StepDisplay.kt
@@ -2,6 +2,7 @@ package com.hci_g1.amelior
 
 import android.content.ComponentName
 import android.content.ServiceConnection
+import android.os.Bundle
 import android.os.IBinder
 import android.view.View
 import android.widget.TextView
@@ -9,21 +10,27 @@ import androidx.fragment.app.Fragment
 
 class StepDisplay: Fragment(R.layout.step_display) {
     /** UI METHODS **/
+    private var fragView: View? = null
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        fragView = view
+    }
+
     fun readout(str: String)
     {
-        TODO(reason = "Implement onCreateView so that View methods work.")
-        //val ro = findViewById<TextView>(R.id.readout)
-        //ro.text = str
+        val ro = fragView?.findViewById<TextView>(R.id.readout)
+        ro?.text = str
     }
 
     /** BINDING THE STEP TRACKER SERVICE **/
-    private var localStepTracker: StepTracker? = null
+    private var STStatus: String? = null
     private var STBound: Boolean = false
 
     private val STConn = object : ServiceConnection {
         override fun onServiceConnected(name: ComponentName, service: IBinder) {
-            val binding = service as StepTracker.STBinding;
-            localStepTracker = binding.getStepTracker()
+            val binding = service as StepTracker.STBinding
+            STStatus = binding.getStepTracker()
             STBound = true
         }
 
