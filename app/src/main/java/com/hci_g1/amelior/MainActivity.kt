1f19a46f6e2115855821dfa92e09afbdcb2e460d
Isabella M <bellamontoya2019@gmail.com>
2022-03-31 11:39:57 -0700

    Added Activity Recognition Permissions.Setup Sensor Manager to detech steps

--- app/src/main/java/com/hci_g1/amelior/MainActivity.kt
+++ app/src/main/java/com/hci_g1/amelior/MainActivity.kt
@@ -7,16 +7,30 @@ import android.os.IBinder
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.add
 import androidx.fragment.app.commit
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.util.Log
+import android.widget.Toast
+import kotlinx.android.synthetic.main.main_activity.*
 
 class MainActivity: AppCompatActivity()
 {
+
+	/**Step Counter Global Variables**/
+	private var sensorManager: SensorManager? = null
+	private var running = false
+	private var totalSteps = 0f
+	private var previousTotalSteps = 0f
+
 	/** LIFECYCLE CALLBACKS **/
 	override fun onCreate(savedInstanceState: Bundle?)
 	{
 		// Recover UI state if this activity was killed
 		// See https://developer.android.com/guide/components/activities/activity-lifecycle#instance-state
 		super.onCreate(savedInstanceState)
-
 		setContentView(R.layout.main_activity)
 
 		// Host the StepDisplay fragment whenever we instantiate this activity
@@ -28,4 +42,6 @@ class MainActivity: AppCompatActivity()
 			}
 		}
 	}
+
 }
+
--- app/src/main/java/com/hci_g1/amelior/StepTracker.kt
+++ app/src/main/java/com/hci_g1/amelior/StepTracker.kt
@@ -1,16 +1,29 @@
 package com.hci_g1.amelior
 
 import android.app.Service
+import android.content.Context
 import android.content.Intent
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Binder
 import android.os.IBinder
+import android.util.Log
+import android.widget.Toast
 
 /* NOTE: Services use the main thread by default
 *   May need to start a thread here so we don't accidentally block UI operations
 *   See https://developer.android.com/guide/components/services#CreatingAService
 */
 
-class StepTracker : Service() {
+class StepTracker : Service(), SensorEventListener
+{
+    /**SENSOR VARIABLES**/
+    private var sensorManager: SensorManager? = null
+    private var running = false
+    private var totalSteps = 0f
+    private var previousTotalSteps = 0f
     /** DATA **/
     private var status: String = "Lorem Ipsum"
     private var demand: Int = 0
@@ -34,6 +47,10 @@ class StepTracker : Service() {
     override fun onCreate() {
         super.onCreate()
         status = "Created new StepTracker\n"
+
+        loadData()
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
@@ -47,6 +64,13 @@ class StepTracker : Service() {
             our service getting killed while starting.
             Come back around after main functionality is done.
          */
+        /**SENSOR SETUP**/
+        val stepSensor = sensorManager?.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)
+        if (stepSensor == null) {
+            Toast.makeText(this, "No sensor detected on this device", Toast.LENGTH_SHORT).show()
+        } else {
+            sensorManager?.registerListener(this, stepSensor, SensorManager.SENSOR_DELAY_UI)
+        }
 
         // We want the Step Tracking to continue ASAP if it gets killed w/o a manual stop
         return START_REDELIVER_INTENT
@@ -77,5 +101,33 @@ class StepTracker : Service() {
         status = "Client Rebound\nNow bound to $demand clients\n"
 
         super.onRebind(intent)
+    }
+
+    /**SENSOR CALLBACKS**/
+    override fun onSensorChanged(event: SensorEvent?) {
+//        var tv_stepsTaken = findViewById<TextView>(R.id.tv_stepsTaken)
+        if (running) {
+            totalSteps = event!!.values[0]
+            val currentSteps: Int = totalSteps.toInt() - previousTotalSteps.toInt()
+            status = ("$currentSteps")
+        }
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+    // Must be implemented for SensorEventListener
+    }
+//
+//    private fun saveData() {
+//        val sharedPreferences = getSharedPreferences("myPreferences", Context.MODE_PRIVATE)
+//        val editor = sharedPreferences.edit()
+//        editor.putFloat("key1", previousTotalSteps)
+//        editor.apply()
+//    }
+
+    private fun loadData() {
+        val sharedPreferences = getSharedPreferences("myPreferences", Context.MODE_PRIVATE)
+        val savedNumber = sharedPreferences.getFloat("key1", 0f)
+        Log.d("MainActivity", "$savedNumber")
+        previousTotalSteps = savedNumber
     }
 }
