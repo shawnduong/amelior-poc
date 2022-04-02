55d46bbf74be84d0e9579b6e93d86d162a4024d8
Shawn Duong <shawnduong@protonmail.com>
2022-04-01 19:08:33 -0700

    Implemented location requesting.

--- app/src/main/java/com/hci_g1/amelior/MainActivity.kt
+++ app/src/main/java/com/hci_g1/amelior/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.hci_g1.amelior
 
+import android.content.pm.PackageManager
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.Manifest
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 
 class MainActivity: AppCompatActivity()
 {
@@ -12,8 +15,25 @@ class MainActivity: AppCompatActivity()
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.main_activity)
 
-		/* Find the GPS button and have it start the buttonGPS service. */
-		val buttonGPS: Button = findViewById(R.id.buttonGPS)
-		buttonGPS.setOnClickListener { startService(Intent(applicationContext, GPS::class.java)) }
+		request_permissions()
+	}
+
+	/* Request permissions. */
+	private fun request_permissions(): Boolean
+	{
+		/* Request LOCATION permissions only if they haven't been granted already. */
+		if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+			!= PackageManager.PERMISSION_GRANTED)
+		{
+			ActivityCompat.requestPermissions(
+				this,
+				arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+				34  /* Foreground only permissions request code */
+			)
+
+			return true
+		}
+
+		return false
 	}
 }
