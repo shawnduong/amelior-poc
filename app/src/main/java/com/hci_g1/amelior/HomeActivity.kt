88fd256454d0f2b7a31e444c1e949e7478d04825
Shawn Duong <shawnduong@protonmail.com>
2022-04-08 12:52:31 -0700

    Refactored HomeActivity.kt

--- app/src/main/java/com/hci_g1/amelior/HomeActivity.kt
+++ app/src/main/java/com/hci_g1/amelior/HomeActivity.kt
@@ -1,23 +1,24 @@
 package com.hci_g1.amelior
-//
-import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.*
 import com.hci_g1.amelior.Model.RecycleViewAdapter
+
+class HomeActivity: AppCompatActivity()
+{
+//	private var layoutManager: RecyclerView.LayoutManager? = null
+//	private var adapter: RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>? = null
 //
-class HomeActivity : AppCompatActivity() {
-//    private var layoutManager: RecyclerView.LayoutManager? = null
-//    private var adapter: RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>? = null
+//	override fun onCreate(savedInstanceState: Bundle?)
+//	{
+//		super.onCreate(savedInstanceState)
+//		setContentView(R.layout.home_activity)
 //
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContentView(R.layout.home_activity)
+//		layoutManager = LinearLayoutManager(this)
+//		recyclerView.LayoutManager = layoutManager
 //
-//        layoutManager = LinearLayoutManager(this)
-//        recyclerView.LayoutManager = layoutManager
-//
-//        adapter = RecycleViewAdapter ()
-//        recycleView.adapter = adapter
-//    }
+//		adapter = RecycleViewAdapter()
+//		recycleView.adapter = adapter
+//	}
 }
