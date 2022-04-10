a4813bfbd5498343a3e0b4a6f5d3f8762d867393
Shawn Duong <shawnduong@protonmail.com>
2022-04-10 15:56:42 -0700

    Converted spaces to tabs in SplashScreen.kt.

--- app/src/main/java/com/hci_g1/amelior/SplashScreen/SplashScreen.kt
+++ app/src/main/java/com/hci_g1/amelior/SplashScreen/SplashScreen.kt
@@ -8,18 +8,18 @@ import com.hci_g1.amelior.MainActivity
 import com.hci_g1.amelior.R
 
 class SplashScreen : AppCompatActivity() {
-    lateinit var handler: Handler
-    private val SPLASH_TIME: Long = 3000
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.splash_screen_activity)
+	lateinit var handler: Handler
+	private val SPLASH_TIME: Long = 3000
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.splash_screen_activity)
 
-        handler = Handler()
-        handler.postDelayed({
-            val intent = Intent(this, MainActivity:: class.java)
-            startActivity(intent)
-            finish()
-        }, SPLASH_TIME) //delaying 3s to open mainactivity
+		handler = Handler()
+		handler.postDelayed({
+			val intent = Intent(this, MainActivity:: class.java)
+			startActivity(intent)
+			finish()
+		}, SPLASH_TIME) //delaying 3s to open mainactivity
 
-    }
+	}
 }
