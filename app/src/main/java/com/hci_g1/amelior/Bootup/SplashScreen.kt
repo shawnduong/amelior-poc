ca74071741e1b615a2639819745982c25cde5f17
Shawn Duong <shawnduong@protonmail.com>
2022-04-12 23:31:10 -0700

    Removed unused import from SplashScreen.kt.

--- app/src/main/java/com/hci_g1/amelior/Bootup/SplashScreen.kt
+++ app/src/main/java/com/hci_g1/amelior/Bootup/SplashScreen.kt
@@ -5,7 +5,6 @@ import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.content.ServiceConnection
-import android.hardware.SensorManager
 import android.os.Bundle
 import android.os.Handler
 import android.os.IBinder
