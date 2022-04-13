b761f7f5c3254567e422902dd594367d68a1042c
Shawn Duong <shawnduong@protonmail.com>
2022-04-12 23:27:08 -0700

    Cleaned up FirstTimeSetup.kt.

--- app/src/main/java/com/hci_g1/amelior/Bootup/FirstTimeSetup.kt
+++ app/src/main/java/com/hci_g1/amelior/Bootup/FirstTimeSetup.kt
@@ -1,31 +1,17 @@
 package com.hci_g1.amelior
 
 import android.animation.ObjectAnimator
-import android.content.pm.PackageManager
-import android.content.ComponentName
-import android.content.Context
-import android.content.Intent
-import android.content.ServiceConnection
-import android.hardware.SensorManager
 import android.os.Bundle
-import android.os.IBinder
 import android.util.DisplayMetrics
-import android.util.Log
 import android.view.KeyEvent
 import android.view.View
-import android.view.WindowManager
 import android.widget.*
-import android.Manifest
 import androidx.appcompat.app.AppCompatActivity
-import androidx.constraintlayout.widget.*
-import androidx.core.app.ActivityCompat
-import androidx.core.view.*
 
 class FirstTimeSetup: AppCompatActivity()
 {
-	private lateinit var buttonNext: Button
-	private lateinit var nameInputField: EditText
-	private lateinit var nameForm: LinearLayout
+	private lateinit var linearLayoutNameForm: LinearLayout
+	private lateinit var editTextNameInputField: EditText
 
 	override fun onCreate(savedInstanceState: Bundle?)
 	{
@@ -34,14 +20,16 @@ class FirstTimeSetup: AppCompatActivity()
 
 
 		/* Initialize widgets. */
-		nameInputField = findViewById(R.id.nameInputField)
-		nameForm = findViewById(R.id.nameForm)
+		linearLayoutNameForm = findViewById(R.id.nameForm)
+		editTextNameInputField = findViewById(R.id.nameInputField)
 
 		/* UI logic. */
-		nameInputField.setOnKeyListener(
+
+		/* Upon finishing typing in the name, move the form field up. */
+		editTextNameInputField.setOnKeyListener(
+
 			View.OnKeyListener { v, key, event ->
 
-				/* Upon finishing typing in the name, move the form field up. */
 				if (key == KeyEvent.KEYCODE_ENTER && event.action == KeyEvent.ACTION_UP)
 				{
 					val displayMetrics = DisplayMetrics()
@@ -49,12 +37,12 @@ class FirstTimeSetup: AppCompatActivity()
 
 					val height = displayMetrics.heightPixels.toFloat()
 
-					ObjectAnimator.ofFloat(nameForm, "translationY", height * -0.33f).apply {
+					ObjectAnimator.ofFloat(linearLayoutNameForm, "translationY", height * -0.33f).apply {
 						duration = 500  // milliseconds
 						start()
 					}
 
-					ObjectAnimator.ofFloat(nameForm, "alpha", 0.25f).apply {
+					ObjectAnimator.ofFloat(linearLayoutNameForm, "alpha", 0.25f).apply {
 						duration = 500  // milliseconds
 						start()
 					}
@@ -65,13 +53,6 @@ class FirstTimeSetup: AppCompatActivity()
 				false
 			}
 		)
-
-//		buttonNext = findViewById(R.id.proceedButton)
-//
-//		/* UI logic. */
-//		buttonNext.setOnClickListener {
-//			startActivity(Intent(this, MainMenuActivity::class.java))
-//		}
 	}
 
 	companion object
