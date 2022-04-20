49c06bc7df53eb533b999becbe6fe97da0a443c4
shawnduong <shawnduong@protonmail.com>
2022-04-20 00:08:39 -0700

    Refactored existing navbar code.

--- app/src/main/java/com/hci_g1/amelior/Dashboard.kt
+++ app/src/main/java/com/hci_g1/amelior/Dashboard.kt
@@ -1,30 +1,36 @@
 package com.hci_g1.amelior
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.Navigation
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.NavigationUI
+
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class Dashboard: AppCompatActivity()
 {
-    override fun onCreate(savedInstanceState: Bundle?)
-    {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.dashboard)
+	override fun onCreate(savedInstanceState: Bundle?)
+	{
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.dashboard)
 
-        /*
-            Get the NavController from the NavHost this activity contains
-            so we can plug in the navigation bar to it.
-        */
-        val navHost = supportFragmentManager.findFragmentById(R.id.NavigationHostFragment)
-        val navController = navHost?.findNavController()
-        val bottomNavBar = findViewById<BottomNavigationView>(R.id.navbar)
-        if (navController != null)
-        {
-            NavigationUI.setupWithNavController(bottomNavBar, navController)
-        }
-    }
+		/* Get the NavController from the navhost this activity contains so we
+		   can plug in the navbar to it. */
+		val navhost = supportFragmentManager.findFragmentById(R.id.navhost)
+		val navController = navhost?.findNavController()
+		val navbar = findViewById<BottomNavigationView>(R.id.navbar)
+
+		if (navController != null)
+		{
+			NavigationUI.setupWithNavController(navbar, navController)
+			Log.d(TAG, "Navigation bar successfully set up.")
+		}
+	}
+
+	companion object
+	{
+		private val TAG = "Dashboard"
+	}
 }
