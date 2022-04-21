14b290c6ce67f6397cd0571f91e3fa5033d7c571
Charison Gill-Branion <cgill-branion@ucmerced.edu>
2022-04-21 13:52:03 -0700

    Add StepCount data class

---
+++ app/src/main/java/com/hci_g1/amelior/databases/entities/StepCount.kt
@@ -0,0 +1,13 @@
+package com.hci_g1.amelior.databases.entities
+
+import androidx.room.*
+
+/* Table name = "StepCount" */
+@Entity
+data class StepCount
+    (
+    @PrimaryKey(autoGenerate=false)
+    val key: Int,
+    val timestamp: Long,
+    val steps: Int
+)
