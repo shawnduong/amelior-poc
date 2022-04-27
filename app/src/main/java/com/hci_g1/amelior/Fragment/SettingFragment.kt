46edabd6890157c95a6731b6a7e23b8eaa7189f5
Crystal <icrystal@Crystals-Macbook.local>
2022-04-26 18:28:29 -0700

    Sample Database

--- app/src/main/java/com/hci_g1/amelior/Fragment/SettingFragment.kt
+++ app/src/main/java/com/hci_g1/amelior/Fragment/SettingFragment.kt
@@ -9,7 +9,9 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.*
 import androidx.navigation.findNavController
+import com.hci_g1.amelior.entities.Distance
 import com.hci_g1.amelior.entities.Mood
+import com.hci_g1.amelior.entities.StepCount
 import com.hci_g1.amelior.entities.User
 import kotlinx.android.synthetic.main.fragment_setting.*
 import kotlinx.android.synthetic.main.fragment_setting.view.*
@@ -21,6 +23,8 @@ class SettingFragment: Fragment() {
 
     private lateinit var userDao: UserDao
     private lateinit var moodDao: MoodDao
+    private lateinit var stepCountDao: StepCountDao
+    private lateinit var distanceDao: DistanceDao
 
     private lateinit var buttonSplashSubmit: Button
     private lateinit var buttonSplashShowInfo: Button
@@ -98,6 +102,7 @@ class SettingFragment: Fragment() {
         }
 
 		/* Debugging buttons. */
+        /* Mood */
 		buttonSampleA.setOnClickListener {
 
 			val context = getContext()
@@ -109,10 +114,180 @@ class SettingFragment: Fragment() {
 				moodDao = UserDatabase.getInstance(context).moodDao
 			}
 
-			Log.d(TAG, "aidjwoiajdoiwajda")
+			Log.d(TAG, "Button A")
 
-			moodDao.insert_mood_now(Mood(0, 1651001400000, 100))
-		}
+            /* 4/22/2022 */
+            moodDao.insert_mood_now(Mood(0, 1650655800000, 10))
+            /* 4/23/2022 */
+            moodDao.insert_mood_now(Mood(1, 1650742200000, 10))
+            /* 4/24/2022 */
+            moodDao.insert_mood_now(Mood(2, 1650828600000, 20))
+            /* 4/25/2022 */
+            moodDao.insert_mood_now(Mood(3, 1650915000000, 40))
+            /* 4/26/2022 */
+            moodDao.insert_mood_now(Mood(4, 1651001400000, 50))
+            /* 4/27/2022 */
+            moodDao.insert_mood_now(Mood(5, 1651087800000, 50))
+            /* 4/28/2022 */
+            moodDao.insert_mood_now(Mood(6, 1651174200000, 70))
+            /* 4/29/2022 */
+            moodDao.insert_mood_now(Mood(7, 1651260600000, 90))
+        }
+
+        /* StepCount */
+        buttonSampleB.setOnClickListener {
+
+            val context = getContext()
+
+            /* Initializing variables. */
+            if (context != null)
+            {
+                userDao = UserDatabase.getInstance(context).userDao
+                moodDao = UserDatabase.getInstance(context).moodDao
+                stepCountDao = UserDatabase.getInstance(context).stepCountDao
+            }
+
+            Log.d(TAG, "Button B")
+            /*
+            /* 4/22/2022 */
+            stepCountDao.insert_step_count_now(StepCount(1650655800000, 100f))
+            /* 4/23/2022 */
+            stepCountDao.insert_step_count_now(StepCount(1650742200000, 150f))
+            /* 4/24/2022 */
+            stepCountDao.insert_step_count_now(StepCount(1650828600000, 200f))
+            /* 4/25/2022 */
+            stepCountDao.insert_step_count_now(StepCount(1650915000000, 250f))
+            /* 4/26/2022 */
+            stepCountDao.insert_step_count_now(StepCount(1651001400000, 300f))
+            /* 4/27/2022 */
+            stepCountDao.insert_step_count_now(StepCount(1651087800000, 350f))
+            /* 4/28/2022 */
+            stepCountDao.insert_step_count_now(StepCount(1651174200000, 400f))
+            /* 4/29/2022 */
+            stepCountDao.insert_step_count_now(StepCount(1651260600000, 450f))
+             */
+
+            /* 4/22/2022 */
+            moodDao.insert_mood_now(Mood(0, 1650655800000, 30))
+            /* 4/23/2022 */
+            moodDao.insert_mood_now(Mood(1, 1650742200000, 20))
+            /* 4/24/2022 */
+            moodDao.insert_mood_now(Mood(2, 1650828600000, 30))
+            /* 4/25/2022 */
+            moodDao.insert_mood_now(Mood(3, 1650915000000, 50))
+            /* 4/26/2022 */
+            moodDao.insert_mood_now(Mood(4, 1651001400000, 60))
+            /* 4/27/2022 */
+            moodDao.insert_mood_now(Mood(5, 1651087800000, 80))
+            /* 4/28/2022 */
+            moodDao.insert_mood_now(Mood(6, 1651174200000, 70))
+            /* 4/29/2022 */
+            moodDao.insert_mood_now(Mood(7, 1651260600000, 90))
+
+        }
+
+
+        /* Distance */
+        buttonSampleC.setOnClickListener {
+
+            val context = getContext()
+
+            /* Initializing variables. */
+            if (context != null)
+            {
+                userDao = UserDatabase.getInstance(context).userDao
+                moodDao = UserDatabase.getInstance(context).moodDao
+                distanceDao = UserDatabase.getInstance(context).distanceDao
+            }
+
+            Log.d(TAG, "Button C")
+            /*
+            distanceDao.insert_distance_now(Distance(1650655800000, 100f))
+            /* 4/23/2022 */
+            distanceDao.insert_distance_now(Distance(1650742200000, 150f))
+            /* 4/24/2022 */
+            distanceDao.insert_distance_now(Distance(1650828600000, 200f))
+            /* 4/25/2022 */
+            distanceDao.insert_distance_now(Distance(1650915000000, 250f))
+            /* 4/26/2022 */
+            distanceDao.insert_distance_now(Distance(1651001400000, 300f))
+            /* 4/27/2022 */
+            distanceDao.insert_distance_now(Distance(1651087800000, 350f))
+            /* 4/28/2022 */
+            distanceDao.insert_distance_now(Distance(1651174200000, 400f))
+            /* 4/29/2022 */
+            distanceDao.insert_distance_now(Distance(1651260600000, 450f))
+            */
+
+            /* 4/22/2022 */
+            moodDao.insert_mood_now(Mood(0, 1650655800000, 20))
+            /* 4/23/2022 */
+            moodDao.insert_mood_now(Mood(1, 1650742200000, 10))
+            /* 4/24/2022 */
+            moodDao.insert_mood_now(Mood(2, 1650828600000, 30))
+            /* 4/25/2022 */
+            moodDao.insert_mood_now(Mood(3, 1650915000000, 50))
+            /* 4/26/2022 */
+            moodDao.insert_mood_now(Mood(4, 1651001400000, 60))
+            /* 4/27/2022 */
+            moodDao.insert_mood_now(Mood(5, 1651087800000, 80))
+            /* 4/28/2022 */
+            moodDao.insert_mood_now(Mood(6, 1651174200000, 70))
+            /* 4/29/2022 */
+            moodDao.insert_mood_now(Mood(7, 1651260600000, 90))
+
+        }
+
+        /* ? */
+        buttonSampleD.setOnClickListener {
+
+            val context = getContext()
+
+            /* Initializing variables. */
+            if (context != null)
+            {
+                userDao = UserDatabase.getInstance(context).userDao
+                moodDao = UserDatabase.getInstance(context).moodDao
+            }
+
+            Log.d(TAG, "Button D")
+            /*
+            distanceDao.insert_distance_now(Distance(1650655800000, 100f))
+            /* 4/23/2022 */
+            distanceDao.insert_distance_now(Distance(1650742200000, 150f))
+            /* 4/24/2022 */
+            distanceDao.insert_distance_now(Distance(1650828600000, 200f))
+            /* 4/25/2022 */
+            distanceDao.insert_distance_now(Distance(1650915000000, 250f))
+            /* 4/26/2022 */
+            distanceDao.insert_distance_now(Distance(1651001400000, 300f))
+            /* 4/27/2022 */
+            distanceDao.insert_distance_now(Distance(1651087800000, 350f))
+            /* 4/28/2022 */
+            distanceDao.insert_distance_now(Distance(1651174200000, 400f))
+            /* 4/29/2022 */
+            distanceDao.insert_distance_now(Distance(1651260600000, 450f))
+            */
+
+            /* 4/22/2022 */
+            moodDao.insert_mood_now(Mood(0, 1650655800000, 10))
+            /* 4/23/2022 */
+            moodDao.insert_mood_now(Mood(1, 1650742200000, 10))
+            /* 4/24/2022 */
+            moodDao.insert_mood_now(Mood(2, 1650828600000, 40))
+            /* 4/25/2022 */
+            moodDao.insert_mood_now(Mood(3, 1650915000000, 50))
+            /* 4/26/2022 */
+            moodDao.insert_mood_now(Mood(4, 1651001400000, 70))
+            /* 4/27/2022 */
+            moodDao.insert_mood_now(Mood(5, 1651087800000, 80))
+            /* 4/28/2022 */
+            moodDao.insert_mood_now(Mood(6, 1651174200000, 90))
+            /* 4/29/2022 */
+            moodDao.insert_mood_now(Mood(7, 1651260600000, 90))
+
+        }
+
     }
 
     companion object
@@ -136,3 +311,18 @@ class SettingFragment : Fragment(R.layout.fragment_setting) {
 }
  */
 
+
+/* STORY: This person is on top of their goals that they've set and
+has been really motivated. */
+
+/* STORY: This person is very inconsistent with their goals
+that they've set. They have been slacking off recently and
+haven't been reaching their goals lately. */
+
+/* STORY: This person is very good at satisfying their goals set for
+walking certain amount of steps, but it seems like they've been slacking off
+on other goals. */
+
+/* STORY: This person sets their goals, but never bothered to even try
+completing their goals.
+*/
